8a178a9bbf059bacb5839b31c5d2ed10ce53aba5
ygrit0s <ygritos@outlook.com>
2022-11-08 21:32:33 +0900

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    UserDao userDao = new UserDaoJDBCImpl();
+    public void createUsersTable() throws SQLException {
+        userDao.createUsersTable();
+    }
+
+    public void dropUsersTable() throws SQLException {
+        userDao.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDao.saveUser(name, lastName, age);
+        System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+    }
+
+    public void removeUserById(long id) throws SQLException {
+        userDao.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        return userDao.getAllUsers();
+
+    }
+
+    public void cleanUsersTable() throws SQLException {
+        userDao.cleanUsersTable();
+    }
+}
